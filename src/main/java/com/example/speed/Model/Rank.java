265d46d77ab5fc2179ced86a50f7fef10081943f
Brendan_Ralston <ralston.brendan@me.com>
2018-10-09 21:52:48 -0600

    Added enum for card rank, Ace considered low

---
+++ src/main/java/com/example/speed/Model/Rank.java
@@ -0,0 +1,18 @@
+package com.example.speed.Model;
+
+public enum Rank {
+    ACE,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
