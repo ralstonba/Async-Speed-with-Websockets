e073c5681303d6fa39643879e6d5d2ffd54a7696
Brendan_Ralston <ralston.brendan@me.com>
2018-10-09 21:43:53 -0600

    Initial Commit

---
+++ src/main/java/com/example/speed/SpeedApplication.java
@@ -0,0 +1,12 @@
+package com.example.speed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedApplication.class, args);
+	}
+}
