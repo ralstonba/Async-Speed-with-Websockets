064785894a1518d30d4764ff3021bffee0e4efac
Brendan_Ralston <ralston.brendan@me.com>
2018-10-10 18:48:13 -0600

    Added empty controller class to allow serving static content (index.html)

---
+++ src/main/java/com/example/speed/Controller/SpeedController.java
@@ -0,0 +1,9 @@
+package com.example.speed.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SpeedController {
+    // A controller (annotated with @Controller) needs to exist to serve static content
+    // TODO: Add game logic stuff
+}
