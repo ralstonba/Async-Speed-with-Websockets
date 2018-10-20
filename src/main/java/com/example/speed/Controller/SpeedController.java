83bed8957b979a55694bea86bce202e7a92937ee
Brendan_Ralston <ralston.brendan@me.com>
2018-10-19 23:05:43 -0600

    Defined playCard handler -playRequest validation --ensures request is well formed --special cases for Ace and King

--- src/main/java/com/example/speed/Controller/SpeedController.java
+++ src/main/java/com/example/speed/Controller/SpeedController.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Controller
@@ -19,13 +20,26 @@ public class SpeedController {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
-    // TODO: Add game logic stuff
 
     @MessageMapping("/rest/api/game.playCard")
     public void playCard(@Payload Action cardMove, @Header("simpSessionId") String sessionID) {
-        // TODO: Update game instance using Action
+        speedInstance = SpeedInstance.getInstance();
+        logger.debug("playCard endpoint hit by user with sessionID: {}", sessionID);
 
-        sendGameState();
+        if (cardMove.getType() == ActionType.PLAY && sessionID != null) {
+
+            if (validatePlayerMove(speedInstance, cardMove, sessionID)) {
+                logger.info("playCard request from {} valid", sessionID);
+                playCardHelper(speedInstance, cardMove, sessionID);
+            } else {
+                logger.info("playCard request from {} invalid, REJECTED", sessionID);
+            }
+
+        } else {
+            logger.debug("playCard request from {} with ActionType {} malformed, REJECTED", sessionID, cardMove.getType());
+        }
+
+        sendGameState(speedInstance);
     }
 
     @MessageMapping("/rest/api/game.drawCard")
@@ -70,5 +84,72 @@ public class SpeedController {
             SanitizedGameState sanitizedGameState = new SanitizedGameState(playerID, thisGameState);
             simpMessagingTemplate.convertAndSendToUser(playerID, "/queuq/gamestate", sanitizedGameState);
         }
+    }
+
+    private boolean validatePlayerMove(SpeedInstance thisGameState, Action cardMove, String sessionID) {
+        Player thisPlayer = speedInstance.getPlayerMap().get(thisGameState);
+
+        if (thisPlayer.getHand().getHand().contains(cardMove.getSource())) {
+            if (Arrays.asList(speedInstance.getPlayOptions()).contains(cardMove.getDestination())) {
+                logger.debug("Player {} selected a valid destination target, checking for acceptance", sessionID);
+
+                    /*
+                        Valid moves are defined as source being +/- 1 from destination
+                        - Special consideration for playing an Ace; it can be played on either a 2 or King
+                        - Special consideration for playing a King; it can be player on either an Ace or Queen
+                    */
+
+                Rank sourceCard = cardMove.getSource().getRank();
+                Rank destinationCard = cardMove.getDestination().getRank();
+
+                switch (sourceCard) {
+                    case TWO:
+                    case THREE:
+                    case FOUR:
+                    case FIVE:
+                    case SIX:
+                    case SEVEN:
+                    case EIGHT:
+                    case NINE:
+                    case TEN:
+                    case JACK:
+                    case QUEEN:
+                        logger.debug("Source card: {}, treating as general case", sourceCard);
+                        if (sourceCard.ordinal() == destinationCard.ordinal() + 1 || sourceCard.ordinal() == destinationCard.ordinal() - 1) {
+                            logger.debug("For Source: {} and Destination: {}, play was valid", sourceCard, destinationCard);
+                            return true;
+                        }
+                        break;
+                    case KING:
+                        logger.debug("Using special case for KING");
+                        if (destinationCard == Rank.QUEEN || destinationCard == Rank.ACE) {
+                            logger.debug("For Source: {} and Destination: {}, play was valid", sourceCard, destinationCard);
+                            return true;
+                        }
+                        break;
+                    case ACE:
+                        logger.debug("Using special case for ACE");
+                        if (destinationCard == Rank.KING || destinationCard == Rank.TWO) {
+                            logger.debug("For Source: {} and Destination: {}, play was valid", sourceCard, destinationCard);
+                            return true;
+                        }
+                        break;
+                    default:
+                        logger.warn("Source Card rank not recognized in playCard");
+                        break;
+                }
+
+            } else {
+                logger.debug("Player {} attempted to play on a card that was not a destination option", sessionID);
+            }
+        } else {
+            logger.debug("Player {} attempted to play a card that is not in their hand", sessionID);
+        }
+
+        return false;
+    }
+
+    private void playCardHelper(SpeedInstance thisGameState, Action cardMove, String sessionID) {
+        // TODO: Remove card from players hand, replace play option with played card
     }
 }
