465891c0aa5e92841a10515e391fb41031908ad2
Brendan_Ralston <ralston.brendan@me.com>
2018-11-05 23:00:40 -0700

    Changed Card to a class -Added constructor to generate image url from rank & suit

--- src/main/resources/static/js/models/card.js
+++ src/main/resources/static/js/models/card.js
@@ -1,1 +1,32 @@
+var Models;
+(function (Models) {
+    var Card = /** @class */ (function () {
+        function Card(rank, suit) {
+            this.rank = rank;
+            this.suit = suit;
+            this.suit = suit;
+            this.rank = rank;
+            this.src = this.srcUrlBuilder(rank, suit);
+        }
+        Card.prototype.srcUrlBuilder = function (rank, suit) {
+            var ranks = ["ACE",
+                "TWO",
+                "THREE",
+                "FOUR",
+                "FIVE",
+                "SIX",
+                "SEVEN",
+                "EIGHT",
+                "NINE",
+                "TEN",
+                "JACK",
+                "QUEEN",
+                "KING"];
+            "img/2SPADES.png";
+            return "img/" + (ranks.indexOf(rank) + 1) + suit + ".png";
+        };
+        return Card;
+    }());
+    Models.Card = Card;
+})(Models || (Models = {}));
 //# sourceMappingURL=card.js.map
--- src/main/resources/static/js/models/card.ts
+++ src/main/resources/static/js/models/card.ts
@@ -1,7 +1,34 @@
 namespace Models {
-    export interface Card {
+    export class Card {
+
         src: string;
         id: string;
-        stack?:number;
+        stack ?: number;
+
+        constructor(private rank: string, private suit: string) {
+            this.suit = suit;
+            this.rank = rank;
+            this.src = this.srcUrlBuilder(rank, suit);
+        }
+
+        srcUrlBuilder(rank: string, suit: string): string {
+            let ranks = ["ACE",
+                "TWO",
+                "THREE",
+                "FOUR",
+                "FIVE",
+                "SIX",
+                "SEVEN",
+                "EIGHT",
+                "NINE",
+                "TEN",
+                "JACK",
+                "QUEEN",
+                "KING"];
+
+            "img/2SPADES.png";
+
+            return `img/${ranks.indexOf(rank) + 1}${suit}.png`;
+        }
     }
 }
